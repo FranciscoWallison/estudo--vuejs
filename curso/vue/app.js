682b31e0a0987f9f9146544871c054cf59a6afac
Wallison <franciscowallison@gmail.com>
2017-06-03 18:00:33 -0300

    Resolução da fase de projeto
    Ciando uma nova estrutura de valida~]ao de contas apagar

--- curso/vue/app.js
+++ curso/vue/app.js
@@ -13,7 +13,7 @@ var app = new Vue({
 			date_due: '',
 			name: '',
 			value: '',
-			done:0
+			done:false
 		},
 		names: [
 			'Conta de luz',
@@ -25,18 +25,22 @@ var app = new Vue({
 			'Gasolina',
 		],
 		bills: [
-			{date_due: '20/08/2017', name: 'Conta de luz', value: 25.99, done:1},
-			{date_due: '21/08/2017', name: 'Conta de água', value: 30.99, done:0},
-			{date_due: '22/08/2017', name: 'Conta de telefone', value: 60.99, done:0},
-			{date_due: '23/08/2017', name: 'Supermecado', value: 110.50, done:1},
-			{date_due: '20/08/2017', name: 'Cartão de crédito', value: 1500, done:0},
-			{date_due: '25/08/2017', name: 'Empréstimo', value: 120.00, done:0},
-			{date_due: '26/08/2017', name: 'Gasolina', value: 120, done:1}
+			{date_due: '20/08/2017', name: 'Conta de luz', value: 25.99, done:true},
+			{date_due: '21/08/2017', name: 'Conta de água', value: 30.99, done:false},
+			{date_due: '22/08/2017', name: 'Conta de telefone', value: 60.99, done:false},
+			{date_due: '23/08/2017', name: 'Supermecado', value: 110.50, done:true},
+			{date_due: '20/08/2017', name: 'Cartão de crédito', value: 1500, done:false},
+			{date_due: '25/08/2017', name: 'Empréstimo', value: 120.00, done:false},
+			{date_due: '26/08/2017', name: 'Gasolina', value: 120, done:true}
 		]
 	},
 	computed: {
-		//Valida se a conta foi para
+		//Valida se a conta foi paga
 		status: function(){
+			if(!this.bills.length){
+				return false;
+			}
+
 			var caunt = 0;
 			for(var i in this.bills){
 				
@@ -44,7 +48,8 @@ var app = new Vue({
 					caunt++;
 				}
 			}
-			return !caunt ? "Nenhuma conta a pagar" : "Existem "+ caunt +" contas a serem  pagas!";
+			
+			return caunt;
 		}//end contas a serem pagas
 	},
 	methods: {
@@ -59,12 +64,12 @@ var app = new Vue({
 			if(this.formType == 'insert'){
 				this.bills.push(this.bill);
 			};
-			//limpando dados do form e criando um nobo objeto
+			//limpando dados do form e criando um novo objeto
 			this.bill = {
 				date_due: '',
 				name: '',
 				value: '',
-				done: 0
+				done: false
 			};
 			this.activedView = 0;
 		},
@@ -72,12 +77,32 @@ var app = new Vue({
 			this.bill = bill;
 			this.activedView = 1;
 			this.formType = 'update';
+		},
+		deleteBill: function(index){
+
+			if(confirm('Deseja exluir esta conta?'))
+			{
+				//array splice
+				this.bills.splice(index, 1);
+			}
+
 		}
 	},
 	// Filtros
   	filters: {
+  		statusGeneral: function( value ){
+  			
+  			if(value === false){
+  				return "Nem uma conta cadastrada";
+  			}
+  			if (!value) {
+  				return "Nem uma conta pagar";
+  			}else{
+  				return "Existem "+value+" comtas para a pagar";
+  			}
+  		},	
   		doneLabel: function (value) {
-	    	if(value == 0){
+	    	if(value == false){
 				return "Não Paga";
 			}else{
 				return "Paga";
