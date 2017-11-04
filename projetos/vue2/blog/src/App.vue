60353c646a3f8ccbca5ab867a86e3bee721dca50
Wallison <franciscowallison@gmail.com>
2017-11-04 20:07:16 -0200

    Adicionando efeitos e estilos

--- projetos/vue2/blog/src/App.vue
+++ projetos/vue2/blog/src/App.vue
@@ -1,41 +1,56 @@
 <template>
-<div class="corpo">
+  <div class="corpo">
 
-    <h1 class="titulo">{{ titulo }}</h1>
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+	<input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
 
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
 
-        <div class="painel">
-          <h2 class="painel-titulo">{{foto.titulo}}</h2>
-          <div class="painel-corpo">
+      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
 
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-
-          </div><!-- fim painel-corpo -->
-        </div><!-- fim painel -->
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
 
       </li>
     </ul>
 
-</div>
-</template>
-
+  </div>
 </template>
 
 <script>
+
+// importando nosso Painel 
+
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
-  
+   components: {
+
+    'meu-painel': Painel
+  },
   data(){
     return {
       titulo: 'Test',
-      fotos: []
+      fotos: [],
+      filtro: ''
     }
   },
   created(){
     this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
+  },
+  computed: {
+	  fotosComFiltro(){
+		  if(this.filtro){
+			  let exp = new RegExp(this.filtro.trim(), 'i');
+			  return this.fotos.filter(foto => exp.test(foto.titulo));
+		  }else {
+			  return this.fotos
+		  }
+	  }
   }
 }
 
@@ -62,26 +77,8 @@ export default {
 	.imagem-responsiva {
 		width: 100%;
 	}
-
-	/* estilo do painel */ 
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+	.filtro {
+		display: block;
+		width: 100%;
+	}
 </style>
