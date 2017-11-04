25c7f7b79de9cb529cedcdb62fae93a73cd7f3e9
Wallison <franciscowallison@gmail.com>
2017-11-04 13:13:39 -0200

    Primeiros passo blog

---
+++ projetos/vue2/README.md
@@ -0,0 +1,30 @@
+# Estudos Vue.js
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+
+npm install vue-cli@2.7.0 -g
+
+# new project
+vue init webpack-simple blog
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+# Anotações 
+```
+
+    Babel é um tranpiler
+    Webpack é um module bundler
+
+    Imterpolação é um associação unidirecional
+        {{ msg }} 
+```
+
---
+++ projetos/vue2/blog/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>      
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      titulo: 'Test',
+      fotos: [
+        {
+          url: 'https://meusanimais.com.br/wp-content/uploads/2015/03/4-gato-enfermo.jpg',
+          titulo: 'gato-fofo'
+        },
+        {
+          url: 'https://meusanimais.com.br/wp-content/uploads/2015/03/4-gato-enfermo.jpg',
+          titulo: 'gato-fofo'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
