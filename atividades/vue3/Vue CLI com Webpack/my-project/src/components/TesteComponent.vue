f3c70b3d062f0d9086d48d6e8a88c2b751b88da3
Wallison <franciscowallison@gmail.com>
2019-07-15 15:29:33 -0300

    Teste component

---
+++ atividades/vue3/Vue CLI com Webpack/my-project/src/components/TesteComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Teste de Component</p>
+        <span>{{ msg }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Curso de Vue js' 
+        }
+    }
+    
+}
+</script>
+
+<style scope>
+p{color: red;}
+</style>
