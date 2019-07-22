7d662d4c62e982c999f9ddc24dedde812e453a1d
Wallison <franciscowallison@gmail.com>
2019-07-22 17:19:31 -0300

    02 - Manipulando Formulários com Vue JS

--- atividades/vue3/Formulários/my-project/src/components/FormComponent.vue
+++ atividades/vue3/Formulários/my-project/src/components/FormComponent.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <form>
-            <input type="text" placeholder="Nome">
+            <input type="text" placeholder="Nome" v-model="userData.name">
             <hr>
-            <input type="text" placeholder="E-mail">
+            <input type="text" placeholder="E-mail" v-model="userData.email">
             <hr>
-            <input type="text" placeholder="Idade">
+            <input type="text" placeholder="Idade" v-model.number="userData.age">
             <hr>
-            <input type="radio" name="sex" value="M"> Masculino |
-            <input type="radio" name="sex" value="F"> Feminino
+            <input type="radio" name="sex" value="M" v-model="userData.sex"> Masculino |
+            <input type="radio" name="sex" value="F" v-model="userData.sex"> Feminino
             <hr>
-            <select>
+            <select  v-model="userData.state">
                 <option value="">Selecione o Estado</option>
                 <option value="SP">São Paulo</option>
                 <option value="RJ">Rio de Janeiro</option>
@@ -18,9 +18,9 @@
             </select>
             <hr>
             <label for="agree">Concordo com os termos de uso</label>
-            <input type="checkbox" id="agree">
+            <input type="checkbox" id="agree" v-model="terms">
             <hr>
-            <textarea cols="30" rows="10"></textarea>
+            <textarea cols="30" rows="10" v-model="description"></textarea>
             <hr>
             <button type="submit">Enviar</button>
         </form>
@@ -29,7 +29,20 @@
 
 <script>
 export default {
-    
+    data(){
+        return {
+            userData: {
+                name: '',
+                email: '',
+                age: '',
+                sex: 'M',
+                state: ''
+            },
+            terms: false,
+            description: ''
+            
+        }
+    }
 }
 </script>
 
