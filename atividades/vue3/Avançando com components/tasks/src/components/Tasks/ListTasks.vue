31f483ea317efefdc2be7e59345cb8911ef760f4
Wallison <franciscowallison@gmail.com>
2019-08-20 11:06:01 -0300

    08 - Listagem das Tarefas com Vue JS
    Download Arquivos Projeto: https://drive.google.com/open?id=1_b94FG3jRPXxpfzzTvElkuwVMatkFtI_

--- atividades/vue3/Avançando com components/tasks/src/components/Tasks/ListTasks.vue
+++ atividades/vue3/Avançando com components/tasks/src/components/Tasks/ListTasks.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>Id.</th>
+                    <th>Nome</th>
+                    <th width="180px">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(task, index) in tasks" :key="index">
+                    <td>{{ task.id }}</td>
+                    <td>{{ task.name }}</td>
+                    <td>
+                        <a href="" class="btn btn-info">Editar</a>
+                        <a href="" class="btn btn-danger">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -8,7 +28,10 @@
 export default {
     data() {
         return {
-            title: 'Lista de Tarefas'
+            title: 'Lista de Tarefas',
+            tasks: [
+                {id: 12, name: "Lavar Roupa"}
+            ]
         }
     },
 }
